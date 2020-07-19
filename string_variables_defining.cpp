111b302d199d00e005cf130eaa100e93c62699f2
Nikhil Srivastava <68149981+Nikhil-Srivastava-Dev@users.noreply.github.com>
2020-07-19 13:53:34 +0530

    Create string_variables_defining.cpp
    very important c++ file for defining string variables in C++

---
+++ string_variables_defining.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    /*
+    Hey Now I can Make string variables also 😎
+    */
+    string name = "Nikhil Srivastava";
+    cout<<"Hello my name is "<<name<<endl;
+
+    return 0;
+}
